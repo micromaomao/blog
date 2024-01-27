0546564d2d6d50120763f2513bd4c6b1fa483754
Mao <m@maowtm.org>
2024-01-27 15:03:46 +0000

    Do some simple code-splitting to work around cloudflare pages file size limit

--- content/maochat-ai/script.ts
+++ content/maochat-ai/script.ts
@@ -1,8 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { Component } from "./embedding-tool";
 import { onready, bind_container } from "js/jsmeta";
 
-onready(() => {
+onready(async () => {
   let elem = bind_container("embedding_tool");
+  elem.innerHTML = "Loading...";
+  let Component = (await import("./embedding-tool")).Component;
+  elem.innerHTML = "";
   createRoot(elem).render(Component());
 });

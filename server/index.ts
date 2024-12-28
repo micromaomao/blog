5d4866cb45b0ebd1436a75432a8852af5d946aed
Mao <m@maowtm.org>
2024-12-28 03:08:36 +0000

    Fix OPTIONS handling so that rate limit message gets shown.

--- server/index.ts
+++ server/index.ts
@@ -15,6 +15,15 @@ app.use(function (req, res, next) {
   res.set("Access-Control-Allow-Methods", "GET, POST");
   res.set("Access-Control-Allow-Headers", "*");
   res.set("Access-Control-Max-Age", "600");
+
+  if (req.method === "OPTIONS") {
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/OPTIONS:
+    // Note: Both 200 OK and 204 No Content are permitted status codes, but some
+    // browsers incorrectly believe 204 No Content applies to the resource and
+    // do not send a subsequent request to fetch it.
+    res.status(200).send("");
+    return;
+  }
   next();
 })
 

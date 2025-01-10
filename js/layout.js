ee9f0a0f89d39993224ed538e542a1123878f997
Mao <m@maowtm.org>
2025-01-10 23:28:58 +0000

    Add a side-dockable toc

--- js/layout.js
+++ js/layout.js
@@ -16,3 +16,48 @@ document.querySelectorAll(".clickable-article").forEach(elem => {
   }
   elem.style.cursor = "pointer";
 });
+
+let toc = document.querySelector(".toc-container");
+if (toc) {
+  let dock_button_container = document.createElement("span");
+  dock_button_container.classList.add("dock-button-container");
+  dock_button_container.append(document.createTextNode("["));
+  let dock_button = document.createElement("a");
+  dock_button.innerText = "dock to side";
+  dock_button.classList.add("dock-button");
+  dock_button_container.appendChild(dock_button);
+  dock_button_container.append(document.createTextNode("]"));
+  toc.querySelector(".title").appendChild(dock_button_container);
+
+  dock_button.addEventListener("click", evt => {
+    toc.classList.toggle("docked");
+    if (toc.classList.contains("docked")) {
+      toc.classList.remove("hidden");
+      toc.style.transition = "none";
+      toc.style.transition = "";
+      dock_button.innerText = "undock";
+      let open_dock_button = document.createElement("div");
+      open_dock_button.classList.add("open-dock-button");
+      open_dock_button.innerText = "table of contents";
+      open_dock_button.addEventListener("click", evt => {
+        toc.classList.toggle("hidden");
+      });
+      toc.appendChild(open_dock_button);
+    } else {
+      dock_button.innerText = "dock to side";
+      toc.querySelector(".open-dock-button").remove();
+      let toc_y = toc.getBoundingClientRect().top;
+      window.scrollTo(0, window.scrollY + toc_y);
+    }
+  });
+
+  document.addEventListener("mousedown", evt => {
+    if (!evt.target.closest(".toc-container") ||
+      (evt.target.tagName == "A" && !evt.target.classList.contains("dock-button"))) {
+      if (toc.classList.contains("docked") && !toc.classList.contains("hidden")) {
+        toc.classList.add("hidden");
+        evt.preventDefault();
+      }
+    }
+  });
+}
